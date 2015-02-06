d2e237a60c0e03d92418dc58d8cbcd96839550cd
gasolin <gasolin@gmail.com>
2015-02-06 23:26:33 +0800

    simple schedule

--- public/js/main.js
+++ public/js/main.js
@@ -13,5 +13,12 @@
     // document.addEventListener('deviceready', this.onDeviceReady, false); // cordova ready
 
     // App.init();
+    var mySpreadsheet = 'https://docs.google.com/spreadsheet/ccc?key=15WHndnmcxDgUcW-zV1l8Sg41lwBycRTY48aYHttWs9U#gid=0';
+//    "https://docs.google.com/spreadsheets/d/15WHndnmcxDgUcW-zV1l8Sg41lwBycRTY48aYHttWs9U/edit?usp=sharing"
+    // Load an entire sheet.
+    $('#statistics').sheetrock({
+      url: mySpreadsheet,
+      sql: "select A,B,C,D,E"
+    });
   });
 }());
